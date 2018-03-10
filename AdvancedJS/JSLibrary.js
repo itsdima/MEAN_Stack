ab8d86699643c187a14346b8b37bb13e5215e386
Eric Pricop <dimitripricop@yahoo.com>
2018-03-09 19:58:25 -0800

    finished js library

--- AdvancedJS/JSLibrary.js
+++ AdvancedJS/JSLibrary.js
@@ -1,28 +1,63 @@
 var _ = {
 
 	map: function(arr, callback){
+	  newArr = [];
 		for(let i = 0; i < arr.length; i++){
-			callback(arr[i]);
+			newArr.push(callback(arr[i]));
 		}
+		return newArr;
 	},
 
-	reduce: function(){
-
+	reduce: function(arr, callback, memo){
+	  var index;
+	  if(memo === undefined){
+	    memo = arr[0];
+	    index = 1;
+	  }
+	  else{
+	     index = 0;
+	  }
+	  var sum = memo; 
+	  for(let i = index; i < arr.length; i++){
+	    sum = callback(sum, arr[i]);
+	  }
+    return sum;
 	},
 
-	find: function(){
-
+	find: function(arr, callback){
+    for(let i = 0; i< arr.length; i++){
+      if(callback(arr[i])===true){
+        return arr[i];
+      }
+    }
+    return false;
 	},
 
-	filter: function(){
-
+	filter: function(arr, callback){
+    var newArr = [];
+    for(let i = 0; i< arr.length; i++){
+      if(callback(arr[i])===true){
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
 	},
 
-	reject: function(){
-
+	reject: function(arr, callback){
+    var newArr = [];
+    for(let i = 0; i<arr.length; i++){
+      if(callback(arr[i])===false){
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
 	}
 
-}
+};
 
+console.log(_.map([1, 2, 3], function(num){ return num * 3; }));
+console.log(_.reduce([4, 1, 2, 3], function(memo, num){ return memo + num; }, 6));
+console.log(_.find([1, 5, 3, 1, 5, 7], function(num){ return num % 2 === 0; }));
+console.log(_.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 === 0; }));
+console.log(_.reject([1, 2, 3, 4, 5, 7], function(num){ return num % 2 === 0; }));
 
-_.map([1, 2, 3], function(num){ return num * 3; });
